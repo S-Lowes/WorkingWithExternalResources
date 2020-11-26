96f3bdd7d193e9a33e10153d88c185c30593a39f
S-Lowes <seblowes@btinternet.com>
2020-11-26 16:13:31 +0000

    added comments to help my understanding #1

--- 02-ConsumingAPIsUsingJavaScript/02-json.parse/main.js
+++ 02-ConsumingAPIsUsingJavaScript/02-json.parse/main.js
@@ -1,12 +1,12 @@
 var xhr = new XMLHttpRequest();
 var data;
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); //Getting data from server, Get or Post most used methods.
 xhr.send();
 
 xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
+    if (this.readyState == 4 && this.status == 200) { //xhr ready state, mozilla dev network lists the 5 states. (4 operation complete)
+        data = this.responseText; // ^ HTTP status code of 200 means; ok, request succeeded, content delivered.
     };
 }
 
